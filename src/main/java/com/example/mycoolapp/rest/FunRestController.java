a4ebd16711be3d0d1ba9e32e76c894b36d2825d3
ylliu <liuming1908@gmail.com>
2023-01-16 11:19:20 +0800

    add a hello rest controller

---
+++ src/main/java/com/example/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,12 @@
+package com.example.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestController {
+    @GetMapping("/")
+    public String sayHello() {
+        return "Hello World! Time on server is " + new java.util.Date();
+    }
+}
